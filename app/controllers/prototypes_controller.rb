e6bed9309409927713a7d42df11ca311abb60888
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2023-12-16 16:08:42 +0900

    seventh commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -39,8 +39,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
        redirect_to prototype_path(prototype.id)
     else
        render :edit, status: :unprocessable_entity
